558a874dccfa0e42991e56e3ab03fd1e4825108a
Carlos Toledo <carlombo@hotmail.com>
2022-07-19 20:24:04 -0400

    Pruebas de botón con JavaScip

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,14 @@
+// Cambio de títlo con JS
 var titulo = document.querySelector('h1')
 titulo.innerHTML = '¡Título h1 cambiado con JS!'
 
-alert('Test de JavaScript')
+// Botón de prueba
+function miFunc() {
+    alert ('Se ha adado click en el botón 001');
+}
+
+// Variables
+
+var minumero = 1111111;
+document.write (minumero);
 

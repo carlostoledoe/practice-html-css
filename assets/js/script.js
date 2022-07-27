5ba99fedfc8b25d2dd0914a5755bc1e7f70970d0
Carlos Toledo <carlombo@hotmail.com>
2022-07-26 21:37:55 -0400

    Scroll Reveral agregado. Smooth Scrolling pendiente

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,43 @@ titulo.innerHTML = '¡Título h1 cambiado con JS!'
 
 // Botón de prueba
 function miFunc() {
-    alert ('Se ha adado click en el botón 001');
+    alert('Se ha adado click en el botón 001');
 }
 
-// Variables
+// Scroll Reveal
+window.sr = ScrollReveal();
+sr.reveal('.navbar', {
+    duration: 2000,
+    origin: 'top',
+    distance: '40px'
+});
 
-var minumero = 1111111;
-document.write (minumero);
+window.sr = ScrollReveal();
+sr.reveal('.color1', {
+    duration: 2000,
+    origin: 'left',
+    distance: '300px',
+    viewFactor: 0.2
+});
 
+window.sr = ScrollReveal();
+sr.reveal('.color2', {
+    duration: 2000,
+    origin: 'right',
+    distance: '300px'
+});
+
+window.sr = ScrollReveal();
+sr.reveal('#foto01', {
+    duration: 3000,
+    delay: 1000
+    });
+
+window.sr = ScrollReveal();
+sr.reveal('#productos', {
+    duration: 2000,
+    origin: 'bottom',
+    distance: '200px'
+    });
+
+// Smooth Scrolling
